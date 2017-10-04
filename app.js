fd791773e49db2f174017acb9e2bf2ed2936ad72
fergmac <f.macconnell@gmail.com>
2017-10-04 16:29:20 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('starting app');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes');
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('Starting Notes.js');
+
+module.exports.addNote = () =>  {
+ console.log('add Note!');
+ return 'New note';
+}
