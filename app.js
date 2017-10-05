6cd2f2043165710b8376fd40769b6f2ffe336871
fergmac <f.macconnell@gmail.com>
2017-10-04 21:58:18 -0700

    update getAll function and add yargs commands for app functions

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log('starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
@@ -6,11 +5,33 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
-var command = argv._[0];
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+}
 
-console.log('Command:', command);
-console.log('Yargs', argv);
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b' 
+}
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions,
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+var command = argv._[0];
 
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
@@ -21,7 +42,9 @@ if (command === 'add') {
         console.log('Note title taken');
     }
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
     var note = notes.getNote(argv.title);
     if (note) {
